7f1c0641bae9b2cf7c9264ae857e0719a1f5564c
Xalemg <xalemg@gmail.com>
2018-09-25 14:09:36 +0200

    Initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENTS_PRICES = {
+    salad: 0.5,
+    cheese: 0.8,
+    meat: 1.3,
+    bacon: 0.6
+}
+
+class BurgerBuilder extends Component{
+    state = {
+        ingredients : {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    }
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedingredients = {
+            ...this.state.ingredients
+        };
+        updatedingredients[type] = updatedCount;
+        const priceAddition = INGREDIENTS_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({ totalPrice: newPrice, ingredients: updatedingredients});
+    }
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <=0) {
+             return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedingredients = {
+            ...this.state.ingredients
+        };
+        updatedingredients[type] = updatedCount;
+        const priceDeduction = INGREDIENTS_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({ totalPrice: newPrice, ingredients: updatedingredients});
+    }
+    render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <=0
+        }
+        return (
+            <Aux>
+                <Burger ingredients = {this.state.ingredients}/>
+                <BuildControls 
+                ingredientAdded ={this.addIngredientHandler}
+                ingredientRemoved = {this.removeIngredientHandler}
+                disabled ={disabledInfo} 
+                price={this.state.totalPrice} />
+            </Aux>
+        )
+    }
+}
+export default BurgerBuilder;
