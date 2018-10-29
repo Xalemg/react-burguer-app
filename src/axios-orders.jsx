83649f8d916532b71f1a74031d72a4d62ba49f84
Xalemg <xalemg@gmail.com>
2018-10-29 19:44:50 +0100

    Añadida la direccion de firebase correcta

--- src/axios-orders.jsx
+++ src/axios-orders.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+    baseURL: 'https://react-my-burger-9f4e9.firebaseio.com/'
 });
 
 export default instance;
